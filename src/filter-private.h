a056a49c6974a5407337988678f144337357d5aa
Peter Hutterer <peter.hutterer@who-t.net>
2014-07-04 09:29:11 +1000

    filter: add a filter-private.h header file
    To keep the implementation of a filter separate from the users of a filter.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/filter-private.h
+++ src/filter-private.h
@@ -20,48 +20,23 @@
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#ifndef FILTER_H
-#define FILTER_H
+#ifndef FILTER_PRIVATE_H
+#define FILTER_PRIVATE_H
 
 #include "config.h"
 
-struct motion_params {
-	double dx, dy;
+#include "filter.h"
+
+struct motion_filter_interface {
+	void (*filter)(struct motion_filter *filter,
+		       struct motion_params *motion,
+		       void *data, uint64_t time);
+	void (*destroy)(struct motion_filter *filter);
 };
 
-struct motion_filter;
+struct motion_filter {
+	struct motion_filter_interface *interface;
+};
 
-void
-filter_dispatch(struct motion_filter *filter,
-		struct motion_params *motion,
-		void *data, uint64_t time);
 
-struct motion_filter *
-create_linear_acceleration_filter(double speed);
-
-typedef double (*accel_profile_func_t)(struct motion_filter *filter,
-				       void *data,
-				       double velocity,
-				       uint64_t time);
-
-struct motion_filter *
-create_pointer_accelator_filter(accel_profile_func_t filter);
-
-void
-motion_filter_destroy(struct motion_filter *filter);
-
-/*
- * Pointer acceleration profiles.
- */
-
-/*
- * Profile similar which is similar to nonaccelerated but with a smooth
- * transition between accelerated and non-accelerated.
- */
-double
-pointer_accel_profile_smooth_simple(struct motion_filter *filter,
-				    void *data,
-				    double velocity,
-				    uint64_t time);
-
-#endif /* FILTER_H */
+#endif
