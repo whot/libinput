6083d73e35c0541c6c9db94af1b64d38f46b7634
Peter Hutterer <peter.hutterer@who-t.net>
2015-06-23 12:45:55 +1000

    filter: use a tmp variable for the accel factor
    No real effect, just makes the diff for debugging printfs smaller.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/filter.c
+++ src/filter.c
@@ -426,11 +426,14 @@ pointer_accel_profile_linear(struct motion_filter *filter,
 	const double max_accel = accel_filter->accel; /* unitless factor */
 	const double threshold = accel_filter->threshold; /* units/ms */
 	const double incline = accel_filter->incline;
+	double factor;
 
 	s1 = 1;
 	s2 = 1 + (speed_in - threshold) * incline;
 
-	return min(max_accel, s2 > 1 ? s2 : s1);
+	factor =  min(max_accel, s2 > 1 ? s2 : s1);
+
+	return factor;
 }
 
 double
