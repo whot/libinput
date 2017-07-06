e77cd69b12d7b571f822bf45b8d63e6f9a06339f
Peter Hutterer <peter.hutterer@who-t.net>
2017-07-07 09:47:06 +1000

    Add safe_strdup()
    Return value is either NULL or a strdup'd string, depending on the input
    value.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/udev-seat.c
+++ src/udev-seat.c
@@ -102,8 +102,7 @@ device_added(struct udev_device *udev_device,
 	evdev_read_calibration_prop(device);
 
 	output_name = udev_device_get_property_value(udev_device, "WL_OUTPUT");
-	if (output_name)
-		device->output_name = strdup(output_name);
+	device->output_name = strdup(output_name);
 
 	return 0;
 }
