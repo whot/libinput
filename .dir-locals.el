9f31346180a82d358a512b3a3cf28aa739982f72
Peter Hutterer <peter.hutterer@who-t.net>
2018-11-13 10:28:17 +1000

    Repository has moved to https://gitlab.freedesktop.org/libinput/libinput

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((nil . ((indent-tabs-mode . t)
-         (tab-width . 8)
-         (fill-column . 80)))
- (c-mode . ((c-basic-offset . 8))))
--- test/build-pedantic.c
+++
@@ -1,9 +0,0 @@
-#include <libinput.h>
-
-/* This is a build-test only */
-
-int
-main(void)
-{
-	return 0;
-}
